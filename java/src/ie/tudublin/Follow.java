d92ac07f6d96eea0e22f5b104c4bcf21412e3feb
Airish Reyes <reyes.air30@gmail.com>
2023-04-26 16:26:16 +0100

    Making class Follow and creating accessor fields for word and count

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,27 @@
+package ie.tudublin;
+
+public class Follow 
+{
+    int count = 0;
+    String word;
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void setCount(in count)
+    {
+        this.count = count;
+    }
+
+    public String getWord()
+    {
+        return word;
+    }
+
+    public void setWord(String word)
+    {
+        this.word = word;
+    }
+    
+}
